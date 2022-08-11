9561ba66476060fda4c77e8030428684922176b4
sangeun jo <sangeun.jo@outlook.kr>
2022-08-11 17:42:14 +0900

    add TodoApi

---
+++ TodoApi/Readme.md
@@ -0,0 +1,3 @@
+### Web API 앱 > 컨트롤러를 사용하여 웹 API 만들기 
+
+https://docs.microsoft.com/ko-kr/aspnet/core/tutorials/first-web-api?view=aspnetcore-6.0&tabs=visual-studio
---
+++ TodoApi/TodoApi/Program.cs
@@ -0,0 +1,28 @@
+using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+{
+    opt.UseInMemoryDatabase("TodoList");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
